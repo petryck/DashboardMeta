920f556b3f55a18e8ac71a6ad1cdbbc16c95f375
Petryck WIlliam <petryck@i9devgroup.com>
2022-03-14 18:08:41 -0300

    Backend
    Feito backend da dashboard 01  ( diaria )

--- public/client.js
+++ public/client.js
@@ -101,9 +101,167 @@ setTimeout(() => {
 }
 
 
-ProcessoNovo()
-setInterval(() => {
-  ProcessoNovo()
-}, 16020);
+// ProcessoNovo()
+// setInterval(() => {
+//   ProcessoNovo()
+// }, 16020);
 
 /*DASHBOARD MENSAL*/
+
+
+function calcular_grafico(valor0, valor1){
+  var calcular_ = ((valor0*180)/valor1);
+ 
+  if(calcular_ > 180){
+    return 180
+  }else{
+    return calcular_;
+  }
+
+  
+}
+
+
+$.ajax({
+  type: 'GET',
+  url: '/diario',
+  contentType: 'application/json',
+  success: function (data) {
+
+
+$('.number_im').html(data.IM +'/'+ data.IM_Meta)
+$('.number_ia').html(data.IA +'/'+ data.IA_Meta)
+$('.number_em').html(data.EM +'/'+ data.EM_Meta)
+
+
+
+var calc_im = 'rotate('+calcular_grafico(data.IM, data.IM_Meta)+'deg'+')';
+var calc_ia = 'rotate('+calcular_grafico(data.IA, data.IA_Meta)+'deg'+')';
+var calc_em = 'rotate('+calcular_grafico(data.EM, data.EM_Meta)+'deg'+')';
+
+$('#im_grafico').css('transform', calc_im)
+$('#ia_grafico').css('transform', calc_ia)
+$('#em_grafico').css('transform', calc_em)
+
+
+
+
+
+
+
+
+
+
+  }
+
+})
+
+var data = new Date();
+var data2 = new Date();
+var min =  new Date(data.setDate(data.getDate() - 5));
+var max =  new Date(data2.setDate(data2.getDate() + 5));
+console.log(min.getDate(), max.getDate())
+var dias = min.getDate();
+var dias_nomes = ["domingo", "Segunda-Feira", "Terça-Feira", "Quarta-Feira", "Quinta-Feira", "Sexta-Feira", "Sábado"]
+
+for (let index = 0; index < 10; index++) {
+
+  dias = new Date(min.setDate(min.getDate() + 1));
+
+  if(index == 0){
+    $('.DiasLateral').html('<div style="display: grid;" class="NumeroDias">'+dias.getDate()+' <span class="NomeMes">FEV</span><span class="NomeDia">Quarta-Feira</span></div>');
+  }else{
+    if(index == 4){
+      $('.DiasLateral').append('<div style="display: block;" class="NumeroDias active">'+dias.getDate()+' <span class="NomeMes active">FEV</span><span class="NomeDia active">'+dias_nomes[dias.getDay() % 7]+'</span></div>');
+    }else{
+      $('.DiasLateral').append('<div style="display: grid;" class="NumeroDias">'+dias.getDate()+' <span class="NomeMes">FEV</span><span class="NomeDia">Quarta-Feira</span></div>');
+    }
+   
+  }
+
+}
+
+
+
+$.ajax({
+  type: 'GET',
+  url: '/semanal',
+  contentType: 'application/json',
+  success: function (data) {
+
+
+
+$('.porcentagem_IM').css('width', data.Porcentagem_EM+'%')
+$('.porcentagem_IA').css('width', data.Porcentagem_IA+'%')
+$('.porcentagem_EM').css('width', data.Porcentagem_IM+'%')
+
+$('.porcentagem_IM').html(data.Porcentagem_EM+'%')
+$('.porcentagem_IA').html(data.Porcentagem_IA+'%')
+$('.porcentagem_EM').html(data.Porcentagem_IM+'%')
+
+$('#MetaMensal1_numero').text(data.IM+'/'+data.IM_Meta)
+
+$('#MetaMensal2_numero').text(data.IA+'/'+data.IA_Meta)
+
+$('#MetaMensal3_numero').text(data.EM+'/'+data.EM_Meta)
+
+
+  }
+
+})
+
+
+$.ajax({
+  type: 'GET',
+  url: '/ultimos',
+  contentType: 'application/json',
+  success: function (data) {
+
+    console.log(data.length)
+    var contagem = 0;
+    data.forEach(element => {
+    var img_modal = '';
+if(element.Modalidade_Processo == 1){
+  img_modal = 'airPlaneIcon';
+}else if(element.Modalidade_Processo == 2){
+  img_modal = 'shipicon';
+}else if(element.Modalidade_Processo == 3){
+  img_modal = 'shipicon';
+}
+
+      var corpo = '<div class="card mb-3" style="max-width: 700px;">'
+      corpo +='<div class="card-body text-dark">'
+      corpo +='<div id="cardFotoSales">'
+      corpo +='<img class="FotoSales" src="http://chronus-dc.ddns.net:8070/chronus/assets/img/colaboradores/116.jpeg">'
+      corpo +='</div>'
+      corpo +='<div id="cardFotoInside">'
+      corpo +='<img class="FotoSales" src="http://chronus-dc.ddns.net:8070/chronus/assets/img/colaboradores/39.jpeg">'
+      corpo +='</div>'
+      corpo +='<h5 class="card-title">Nº <span class="TituloProcesso">IM1008-22 </span>'
+      corpo +='<span class="DataProcesso"> 09.03.22 - 22:44H</span>'
+      corpo +='</h5>'
+      corpo +='<p class="card-text">Sales - MATEUS RIBEIRO</p>'
+      corpo +='<p class="card-text">Inside sales - PETRYCK WILLIAN</p>'
+      corpo +='<img class="iconsProcesso" src="icons/'+img_modal+'.svg">'
+      corpo +='</div>'
+      corpo +='</div>'
+
+
+
+      if(contagem == 0){
+        $('.TopProcesso').html(corpo)
+     
+      }else{
+        $('.TopProcesso').append(corpo)
+     
+      }
+       
+       
+      contagem++;
+    });
+  
+
+
+  }
+
+})
--- server/server.js
+++ server/server.js
@@ -5,18 +5,64 @@ const app = express()
 import path from 'path'
 import cors from 'cors'
 import http from 'http'
-
 const server = http.Server(app);
 import { dirname } from 'path'
 import { fileURLToPath } from 'url'
 import { Console } from 'console'
-
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 const port = 3560
 
+
+
+
+// const sql = require('mssql');
+import sql from 'mssql'
+const connStr2 = "Server=CONLINE.SQL.HEADCARGO.COM.BR,9322;Database=headcargo_conline;User Id=hc_conline_consulta;Password=3C23D35C-84F4-4205-80A2-D59D58A81BEF;";
+
+
+const connStr = {
+  user: 'hc_conline_consulta',
+  password: '3C23D35C-84F4-4205-80A2-D59D58A81BEF',
+  database: 'headcargo_conline',
+  port:9322,
+  server: 'CONLINE.SQL.HEADCARGO.COM.BR',
+  pool: {
+    max: 99999,
+    min: 0,
+    idleTimeoutMillis: 90000
+  },
+  options: {
+    encrypt: true, // for azure
+    trustServerCertificate: true // change to true for local dev / self-signed certs
+  }
+}
+
+//fazendo a conexão global
+// sql.connect(connStr)
+//    .then(conn => global.conn = conn)
+//    .catch(err => console.log(err));
+
+
+  //  sql.connect(connStr)
+  //  .then(conn => console.log("conectou!"))
+  //  .catch(err => console.log("erro! " + err));
+
+//fazendo a conexão global
+sql.connect(connStr)
+   .then(conn => global.conn = conn)
+   .catch(err => console.log(err));
+
+
+import bodyParser from 'body-parser';
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
 const io = geckos()
 app.use(cors())
+
 app.use('/', express.static(path.join(__dirname, '../public')))
 
 var connection = mysql.createConnection({
@@ -99,12 +145,139 @@ io.onConnection(channel => {
 })
 
 
-app.get('/', (req, res) => {
+
+  app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'))
   })
 
-app.get('/login', (req, res) => {
+  app.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/login.html'))
+  })
+
+
+  app.get('/diario', (req, res) => {
+
+    // const d = new Date();
+    // var day = DataHoje.getDay();
+    // console.log(day)
+
+    var d = new Date(),
+    month = '' + (d.getMonth() + 1),
+    day = '' + d.getDate(),
+    year = d.getFullYear();
+
+    if (month.length < 2) 
+        month = '0' + month;
+    if (day.length < 2) 
+        day = '0' + day;
+
+    global.conn.request()
+    .query(`SELECT * FROM vis_Metas_Diaria WHERE Dia_Abertura = ${day} AND Mes_Abertura = ${month} AND Ano_Abertura = ${year}`)
+    .then(result => {
+
+      res.json(result.recordset[0])
+    })
+    .catch(err => {
+      console.log(err)
+      return err;
+    });
+
+  
+
+  })
+
+  function getYearlyWeekNumber(date){
+    let splitedDate = date.split("-")
+    let dateObj = new Date(+splitedDate[0], +splitedDate[1]-1, +splitedDate[2], 0,0,0,0 )
+    let firstDayYear = new Date(+splitedDate[0],0,1,0,0,0,0 )
+    let yearDay = ((dateObj - firstDayYear) / 86400000)+1
+    let weekInYear = +(String((yearDay + firstDayYear.getDay()+1) / 7).split(".")[0])
+    return { date, yearDay, weekInYear }
+   }
+
+  app.get('/semanal', (req, res) => {
+
+    // const d = new Date();
+    // var day = DataHoje.getDay();
+    // console.log(day)
+
+    var d = new Date(),
+    month = '' + (d.getMonth() + 1),
+    day = '' + d.getDate(),
+    year = d.getFullYear();
+
+    var semana = getYearlyWeekNumber(year+'-'+month+'-'+day).weekInYear+1
+
+
+    if (month.length < 2) 
+        month = '0' + month;
+    if (day.length < 2) 
+        day = '0' + day;
+
+    global.conn.request()
+    .query(`SELECT * FROM vis_Metas_Semanal WHERE Semana_Abertura = ${semana} AND Mes_Abertura = ${month} AND Ano_Abertura = ${year}`)
+    .then(result => {
+    
+      res.json(result.recordset[0])
+    })
+    .catch(err => {
+      console.log(err)
+      return err;
+    });
+
+  
+
+  })
+
+
+  app.get('/mensal', (req, res) => {
+
+    global.conn.request()
+    .query(`SELECT * FROM vis_Metas_Mensal`)
+    .then(result => {
+     
+      res.json(result.recordset)
+    })
+    .catch(err => {
+      console.log(err)
+      return err;
+    });
+
+  
+
+  })
+
+  app.get('/ultimos', (req, res) => {
+
+    global.conn.request()
+    .query(`SELECT TOP 5
+    Cast(Lhs.Data_Abertura_Processo as Datetime) as data_Abertura_Convertido,
+    Lhs.Numero_Processo,
+    Lhs.Data_Abertura_Processo,
+    Vnd.Nome as Vendedor,
+    Ins.Nome as InsideSales,
+    Lms.Modalidade_Processo /*Aereo / MAritimo / Terrestre*/
+  From
+    mov_Logistica_House Lhs
+  JOIN
+    mov_Logistica_Master Lms on Lms.IdLogistica_Master = Lhs.IdLogistica_Master
+  Left Outer JOIN
+    vis_Funcionario Vnd on Vnd.IdPessoa = Lhs.IdVendedor
+  Left Outer JOIN
+    mov_Projeto_Atividade_Responsavel Par on Par.IdProjeto_Atividade = Lhs.IdProjeto_Atividade and (Par.IdPapel_Projeto = 12)
+  Left Outer JOIN
+    vis_Funcionario Ins on Ins.IdPessoa = Par.IdResponsavel ORDER BY Lhs.Data_Abertura_Processo DESC`)
+    .then(result => {
+     
+      res.json(result.recordset)
+    })
+    .catch(err => {
+      console.log(err)
+      return err;
+    });
+
+  
+
   })
 
 
